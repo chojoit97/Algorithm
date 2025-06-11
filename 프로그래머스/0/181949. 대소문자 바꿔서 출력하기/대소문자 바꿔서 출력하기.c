ae46f8defde4f1b201bc4362390fb4c7f8d9cebb
chojoit97 <chojoit@gmail.com>
2025-06-11 09:43:09 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 3.15 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.c
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
-
 #define LEN_INPUT 21
 
 int main(void) {
     char s1[LEN_INPUT];
-    char s2[27] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    char s3[27] = "abcdefghijklmnopqrstuvwxyz";
-
     scanf("%s", s1);
 
-    for (int i = 0; s1[i] != '\0'; i++) {
-        for (int k = 0; k < 27; k++) {
-            if (s1[i] == s2[k]) {
-                printf("%c", s3[k]);
-                break;  // 대문자 변환 끝났으면 멈춤
-            }
-            if (s1[i] == s3[k]) {
-                printf("%c", s2[k]);
-                break;  // 소문자 변환 끝났으면 멈춤
-            }
+    for(int i = 0; s1[i] != '\0'; i++) {
+        if (s1[i] >= 'a' && s1[i] <= 'z') {
+            printf("%c", s1[i] - 32);  // 소문자 → 대문자
+        } else if (s1[i] >= 'A' && s1[i] <= 'Z') {
+            printf("%c", s1[i] + 32);  // 대문자 → 소문자
         }
     }
 
