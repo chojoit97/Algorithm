7a4ff98abc8f5297b4661ff396c94bc7b63802f7
chojoit97 <chojoit@gmail.com>
2025-06-13 10:09:45 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.c
@@ -0,0 +1,22 @@
+/*
+문자들이 담겨있는 배열 arr가 주어집니다. arr의 원소들을 순서대로 이어 붙인 문자열을 return 하는 solution함수를 작성해 주세요.
+1 ≤ arr의 길이 ≤ 200
+arr의 원소는 전부 알파벳 소문자로 이루어진 길이가 1인 문자열입니다.
+arr	result
+["a","b","c"]	"abc"
+*/
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* arr[], size_t arr_len) {
+    int i;
+    // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
+    char* answer = (char*)malloc(arr_len);
+    for(i = 0; i < arr_len; i++)
+        answer[i] = *arr[i];
+    answer[i] = '\0';
+    return answer;
+}
